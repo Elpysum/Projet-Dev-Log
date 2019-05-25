3d16015f05d1013166fe2a740fa31d07fe0c1d2f
AnasBenny <49567763+AnasBenny@users.noreply.github.com>
2019-05-25 21:24:12 +0200

    Add files via upload

--- Code Python/utils.py
+++ Code Python/utils.py
@@ -68,5 +68,31 @@ def step_i(Z_initial, iter = 0):
     plt.title("Affichage de l'itération " + str(iter))
     
 
+from numba import jit
+@jit(nopython=True)
+def calcul_nb_voisins_rapide(Z):
+    forme = len(Z), len(Z[0])
+    N = [[0, ] * (forme[0]) for i in range(forme[1])]
+    for x in range(1, forme[0] - 1):
+        for y in range(1, forme[1] - 1):
+            N[x][y] = Z[x-1][y-1]+Z[x][y-1]+Z[x+1][y-1] \
+            + Z[x-1][y] + 0 +Z[x+1][y] \
+            + Z[x-1][y+1]+Z[x][y+1]+Z[x+1][y+1]
+    return N
+
+
+
+@jit(nopython=True)
+def iteration_jeu_rapide(Z):
+    forme = len(Z), len(Z[0])
+    N = calcul_nb_voisins_rapide(Z)
+
+    for x in range(1,forme[0]-1):
+        for y in range(1,forme[1]-1):
+            if Z[x][y] == 1 and (N[x][y] < 2 or N[x][y] > 3):
+                Z[x][y] = 0
+            elif Z[x][y] == 0 and N[x][y] == 3:
+                Z[x][y] = 1
+    return Z
 
 ########################################################### EXERCICE 2 #########################################################
